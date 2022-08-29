ddaa752b7c933f0256eb4b4a8eb4721058c47dbd
cpheighter <cheighterjr@gmail.com>
2022-08-29 16:27:22 -0400

    fixed for loop

--- API_And_JSON_Exercise/Program.cs
+++ API_And_JSON_Exercise/Program.cs
@@ -1,17 +1,19 @@
 ﻿using Newtonsoft.Json.Linq;
 
-var client1 = new HttpClient();
-var kanyeURL = "https://api.kanye.rest";
-var kanyeResponse = client1.GetStringAsync(kanyeURL).Result;
-
-var client2 = new HttpClient();
-var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-var ronResponse = client2.GetStringAsync(ronURL).Result;
-
 for (int i = 0; i < 5; i++)
 {
+    var client1 = new HttpClient();
+    var kanyeURL = "https://api.kanye.rest";
+    var kanyeResponse = client1.GetStringAsync(kanyeURL).Result;
+
+    var client2 = new HttpClient();
+    var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+    var ronResponse = client2.GetStringAsync(ronURL).Result;
+
+
     var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
-    Console.WriteLine(ronQuote);
+    Console.WriteLine($"Ron:{ronQuote}");
     var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
-    Console.WriteLine(kanyeQuote);
+    Console.WriteLine($"Kanye:{kanyeQuote}");
+    Console.WriteLine();
 }
